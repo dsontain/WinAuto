6ccb7fbdd297713de297ebad030777c64421046e
dsontain <dsontain@users.noreply.github.com>
2019-05-13 17:24:07 +0800

    Update Test_Basic.py

--- Test_Basic.py
+++ Test_Basic.py
@@ -5,8 +5,9 @@ import sys
 #import diskpart_tool
 import diskpart_new
 import logging
+import win32api
 
-
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - [%(levelname)s] - %(message)s')
 
 class Benchmark(unittest.TestCase):
 
@@ -21,21 +22,41 @@ class Benchmark(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        with open("run.json",'r',encoding='utf-8') as json_file:
-                #json.dump(tool_dic,json_file,ensure_ascii=False,indent=4)
-                run_dic = json.load(json_file)  
-        self.target = run_dic["Partition"]
-        self.disk = run_dic["Target"]
-        run_path = os.path.join(os.getcwd(),time.strftime('win_bench_%Y-%m-%d-%H-%M-%S',time.localtime(time.time())))
+        # with open("run.json",'r',encoding='utf-8') as json_file:
+        #         #json.dump(tool_dic,json_file,ensure_ascii=False,indent=4)
+        #         run_dic = json.load(json_file)
+        # self.target = run_dic["Partition"]
+        # self.disk = run_dic["Target"]
+        a = diskpart_new.Diskpart()
+        for k in a.list_disk():
+            print(f"disk : {k}")
+        while True:
+            self.disk = int(input("Select test disk："))
+            if a.system_disk_check(self.disk):
+                print("Do not select system disk!")
+            else:
+                logging.info(f"Test disk : {self.disk}")
+                break
+
+        print(win32api.GetLogicalDriveStrings().split(":\\\x00"))
+
+
+        while True:
+            self.target = input("Partition( such as G ) :")
+            if self.target in win32api.GetLogicalDriveStrings().split(":\\\x00"):
+                print(f"{self.target} already exists, please input a new partition")
+            else:
+                logging.info(f"Test partition : {self.target}")
+                break
+        tag = input("tag:")
+        run_path = os.path.join(os.getcwd(), time.strftime(f'win_bench_{tag}_%Y%m%d-%H%M%S',time.localtime(time.time())))
         os.mkdir(run_path)
         os.chdir(run_path)
-        a = diskpart_new.Diskpart()
         a.select_disk(self.disk)
         a.create_partition_primary(self.disk, self.target)
         a.quit_diskpart()
-        
-        
-        get_DiskInfo(self.disk, "start")
+
+        get_DiskInfo(self.disk)
     
     @classmethod
     def tearDownClass(self):
@@ -43,11 +64,11 @@ class Benchmark(unittest.TestCase):
         a.select_disk(self.disk)
         a.create_partition_primary(self.disk, self.target)
         a.quit_diskpart()
-        cmd = 'start "aa" "{}"'.format("C:\Program Files\Futuremark\PCMark 7\bin\PCMark7.exe")
-        os.system(cmd)
-        time.sleep(5)
-        cmd = 'start "aa" "{}"'.format("C:\Program Files\Futuremark\PCMark 8\bin\PCMark8.exe")
-        os.system(cmd)
+        # cmd = 'start "aa" "{}"'.format("C:\Program Files\Futuremark\PCMark 7\bin\PCMark7.exe")
+        # os.system(cmd)
+        # time.sleep(5)
+        # cmd = 'start "aa" "{}"'.format("C:\Program Files\Futuremark\PCMark 8\bin\PCMark8.exe")
+        # os.system(cmd)
         #get_DiskInfo(self.disk, "end")
 
     #@unittest.skip("demonstrating skipping")
